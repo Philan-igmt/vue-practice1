e4f74171eb074d2a2ef451e431ad7ca6c5d92af1
Philan-igmt <philanisithembiso@gmail.com>
2020-12-07 14:31:06 +0200

    the basics, form,button,comments and variables

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="card">
+  <div class="image">
+    <img src="https://picsum.photos/200/200" alt="">
+  </div>
+  <div class="cont">
+    <h1>Lorem, ipsum dolor </h1>
+    <p v-if="statement">{{name}} ipsum dolor, sit amet consectetur adipisicing elit. Debitis consectetur quisquam corrupti voluptas nulla officiis ipsum laborum sequi delectus similique nisi, vitae eligendi. Voluptatum natus illo praesentium dicta rem excepturi?</p>
+  </div>
+
+  <!-- looping through an array -->
+  <div class="looping">
+  <ul>
+    <li v-for="person in array" :key="person.age">
+      {{person.name}} : {{person.age}}
+    </li>
+  </ul>
+</div>
+
+<!-- form -->
+  <div class="form">
+    <input type="text" v-model="namee">
+    <input type="number" v-model="age">
+    <button :class="dis"  @click="hello">add to list</button>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+
+  name: 'HelloWorld',
+  props: {
+    msg: String
+
+  },
+  data:function(){
+    return{
+      name:'Philani',
+      statement:true,
+      namee:'',
+      age:'',
+      
+      array:[
+        {name:'Philani', age:22},
+        {name:'Philile', age:23},
+        {name:'Sithembiso', age:24},
+        {name:'Nomonde', age:25},
+        {name:'Mhayise', age:26},
+      ]
+    }
+  },
+  methods:{
+    hello:function(){
+      this.array.push({name:this.namee,age:this.age});
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="css" scoped>
+  .card{
+    width: 200px;
+    border: 2px solid black;
+    border-radius: 30px;
+    padding: 20px;
+
+  }
+  .image{
+    display: flex;
+    justify-content: center;
+  }
+ 
+</style>
